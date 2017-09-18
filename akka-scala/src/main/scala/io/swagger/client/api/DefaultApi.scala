6c5bca0f626bbbd54abf1d04fbce337a92d31d5e
gustavorps <email+swaggerhub.com@gustavorps.net>
2017-09-18 01:31:32 +0000

    Generated code by SwaggerHub

--- akka-scala/src/main/scala/io/swagger/client/api/DefaultApi.scala
+++ akka-scala/src/main/scala/io/swagger/client/api/DefaultApi.scala
@@ -20,7 +20,7 @@ object DefaultApi {
    *   code 200 : Seq[Demand] (successful operation)
    */
   def demandsGet(): ApiRequest[Seq[Demand]] =
-    ApiRequest[Seq[Demand]](ApiMethods.GET, "https://virtserver.swaggerhub.com/argonautscrew/ongbook-api-v2/1.0.0", "/demands", "application/json")
+    ApiRequest[Seq[Demand]](ApiMethods.GET, "https://virtserver.swaggerhub.com/argonautscrew/ongbook/2.0.0", "/demands", "application/json")
       .withSuccessResponse[Seq[Demand]](200)
         /**
    * This is an example opeartion to show how security is applied to the call.
@@ -29,7 +29,7 @@ object DefaultApi {
    *   code 200 : Seq[NGO] (successful operation)
    */
   def ngosGet(): ApiRequest[Seq[NGO]] =
-    ApiRequest[Seq[NGO]](ApiMethods.GET, "https://virtserver.swaggerhub.com/argonautscrew/ongbook-api-v2/1.0.0", "/ngos", "application/json")
+    ApiRequest[Seq[NGO]](ApiMethods.GET, "https://virtserver.swaggerhub.com/argonautscrew/ongbook/2.0.0", "/ngos", "application/json")
       .withSuccessResponse[Seq[NGO]](200)
       
 
