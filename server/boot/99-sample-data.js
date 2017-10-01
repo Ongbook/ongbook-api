c4d6a344158c0155db8fec723a78a0367a2b05a7
GustavoRPS <email+tech@gustavorps.net>
2017-10-01 19:13:07 -0300

    init fixture component to populate database #8 #7 #6 #4

---
+++ server/boot/99-sample-data.js
@@ -0,0 +1,9 @@
+module.exports = function (api) {
+  api.loadFixtures()
+  .then(function() {
+    console.log('Fixtures Done!');
+  })
+  .catch(function(err) {
+    console.log('Errors:', err);
+  });
+};
